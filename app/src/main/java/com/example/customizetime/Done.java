b42a537ad495ac46034f8bd6ffad16b12491c82a
Hamayunhanif <hamayunhanif121@gmail.com>
2022-05-16 09:12:10 -0700

    TimeTableProject

--- app/src/main/java/com/example/customizetime/Done.java
+++ app/src/main/java/com/example/customizetime/Done.java
@@ -1,14 +1,33 @@
 package com.example.customizetime;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class Done extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView nav_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolbar);
+        getSupportActionBar().setTitle("Blood Donation App");
+        nav_view = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(Done.this,drawerLayout,
+                toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
     }
 }
--- app/src/main/java/com/example/customizetime/SplashScreen.java
+++ app/src/main/java/com/example/customizetime/SplashScreen.java
@@ -2,14 +2,49 @@ package com.example.customizetime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+    private ImageView logo;
+    private TextView slogan ,title1;
+
+    Animation  topAnimation ,bottomAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.slogan);
+        title1 = findViewById(R.id.title1);
+
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo.setAnimation(topAnimation);
+        title1.setAnimation(bottomAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+        int SPLASH_SCREEN = 4300;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in7 = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(in7);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
 
     }
 }
